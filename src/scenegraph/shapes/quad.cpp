c9ad89784b90a980e16d0dc0c7eae069d212f3ae
yifan <ivenaieseccqu@gmail.com>
2017-12-10 16:35:14 -0500

    turn quad to full screen quad

--- src/scenegraph/shapes/quad.cpp
+++ src/scenegraph/shapes/quad.cpp
@@ -2,15 +2,14 @@
 
 Quad::Quad()
 {
-    v = {-1.f, 1.f,  .0f, .0f, .0f,
-         -1.f, -1.f, .0f, .0f, 1.f,
-         1.f,  1.f,  .0f, 1.f, .0f,
-         1.f,  -1.f, .0f, 1.f, 1.f};
+    v = { -1.0f,  1.0f,   0.0f, 1.0f,
+          -1.0f, -1.0f,   0.0f, 0.0f,
+           1.0f,  1.0f,   1.0f, 1.0f,
+           1.0f, -1.0f,   1.0f, 0.0f };
     int vertexCount = v.size();
     setVertexData(&v[0], v.size(),VBO::GEOMETRY_LAYOUT::LAYOUT_TRIANGLE_STRIP, vertexCount);
-    setAttribute(CS123::GL::ShaderAttrib::POSITION, 3, 0, VBOAttribMarker::DATA_TYPE::FLOAT, false);
-    setAttribute(CS123::GL::ShaderAttrib::NORMAL, 3, 3*sizeof(float), VBOAttribMarker::DATA_TYPE::FLOAT, false);
-    setAttribute(CS123::GL::ShaderAttrib::TEXCOORD0, 2, 6*sizeof(float), VBOAttribMarker::DATA_TYPE::FLOAT, false);
+    setAttribute(CS123::GL::ShaderAttrib::POSITION, 2, 0, VBOAttribMarker::DATA_TYPE::FLOAT, false);
+    setAttribute(CS123::GL::ShaderAttrib::TEXCOORD0, 2, 2*sizeof(float), VBOAttribMarker::DATA_TYPE::FLOAT, false);
     buildVAO();
 }
 
